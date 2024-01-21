d35fead018300c7043cd3d54a98198880fc6b4e1
Adarsh Rangare <akrangare@gmail.com>
2024-01-21 11:44:09 +0530

    1st try

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,22 @@
 import { v4 as uuidv4 } from "uuid";
 
-const reducer = (state, action) => {};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add": {
+      action.setBalance((prev) => prev + 10);
+
+      return [...state, {id:uuidv4(), name:action.name}];
+    }
+    case "fuel": {
+        
+        if(action.balance >=50){
+            action.setBalance(prev=>prev-50);
+        }
+
+        return [...state];
+
+    }
+  }
+};
 
 export default reducer;
